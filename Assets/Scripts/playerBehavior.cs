2893bcf18937cf714c9d9d878b83f16e36f6f2bc
Aluno <aluno@naverecife.local>
2019-03-18 13:07:48 -0300

    Primeiro commit

---
+++ Assets/Scripts/playerBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerBehavior : MonoBehaviour
+{
+    float moviment;
+
+    public float speed;
+
+    void Update()
+    {
+        move();
+    }
+
+    void move()
+    {
+        moviment = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.right * moviment * speed * Time.deltaTime);
+    }
+}
