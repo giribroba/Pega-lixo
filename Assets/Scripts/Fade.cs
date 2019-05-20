e5d36d8f74ab4531cbc6b722e9646f2aeeea5775
giribroba <giribroba@gmail.com>
2019-05-19 23:29:43 -0300

    Fade (Em andamento...)

---
+++ Assets/Scripts/Fade.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fade : MonoBehaviour
+{
+    [SerializeField] float sensibilidade, midOpa;
+    [SerializeField] Image fade;
+    Color tempColor;
+
+    void Start()
+    {
+        tempColor = fade.color;
+        tempColor.a = 0.75f;
+    }
+
+    void Update()
+    {
+        fade.color = tempColor;
+        tempColor.a -= ((fade.color.a > midOpa) ? sensibilidade * Time.deltaTime : 0);
+    }
+}
